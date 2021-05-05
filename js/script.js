b3b8ad6a10ac8ecfd324973047d29dfa770b40fe
Xunin <>
2021-05-05 15:54:10 +0800

    omi 改版01

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ $(function() {
                     'background-color': '#ffffff20',
                     'backdrop-filter': 'blur(10px)',
                     '-webkit-backdrop-filter': 'blur(10px)',
-                    '-moz-backdrop-filter': 'blur(10px)'
+                    '-moz-backdrop-filter': 'blur(10px)',
+                    'height': '60px',
                 })
                 // $('header img').attr('src', './img/logo1.svg')
         } else {
@@ -15,12 +16,27 @@ $(function() {
                     'background-color': 'transparent',
                     'backdrop-filter': 'blur(0px)',
                     '-webkit-backdrop-filter': 'blur(0px)',
-                    '-moz-backdrop-filter': 'blur(0px)'
+                    '-moz-backdrop-filter': 'blur(0px)',
+                    'height': '100px'
                 })
                 // $('header img').attr('src', './img/logo2.svg')
 
         }
     })
+    var block1 = $('.sec1').offset().top;
+    var logo = $('.kv').offset().top;
 
+    $('.scroller').click(function() {
+        $('html,body').animate({ scrollTop: block1 }, 1000)
+        return false;
+    })
+    $('header .logo').click(function() {
+        $('html,body').animate({ scrollTop: logo }, 1000)
+        return false;
+    })
+    $('footer .logo').click(function() {
+        $('html,body').animate({ scrollTop: logo }, 0)
+        return false;
+    })
 
 });
