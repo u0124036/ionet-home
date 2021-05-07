c028450e4c511ae5ec4d2558f8e7efb26abc1270
Xunin <>
2021-05-07 11:02:29 +0800

    add scroll menu

--- js/script.js
+++ js/script.js
@@ -1,30 +1,21 @@
 $(function() {
+    var n = $(window).scrollTop();
+
+    if (n >= 150) {
+        collapsMenu();
+    }
     $(window).scroll(function() {
         var n = $(window).scrollTop();
         console.log(n);
         if (n >= 150) {
-            $('header').css({
-                    'background-color': '#ffffff20',
-                    'backdrop-filter': 'blur(10px)',
-                    '-webkit-backdrop-filter': 'blur(10px)',
-                    '-moz-backdrop-filter': 'blur(10px)',
-                    'height': '60px',
-                })
-                // $('header img').attr('src', './img/logo1.svg')
+            collapsMenu();
         } else {
-            $('header').css({
-                    'background-color': 'transparent',
-                    'backdrop-filter': 'blur(0px)',
-                    '-webkit-backdrop-filter': 'blur(0px)',
-                    '-moz-backdrop-filter': 'blur(0px)',
-                    'height': '100px'
-                })
-                // $('header img').attr('src', './img/logo2.svg')
-
+            expendMenu();
         }
     })
     var block1 = $('.sec1').offset().top;
     var logo = $('.kv').offset().top;
+    var footer = $('footer').offset().top;
 
     $('.scroller').click(function() {
         $('html,body').animate({ scrollTop: block1 }, 1000)
@@ -39,4 +30,33 @@ $(function() {
         return false;
     })
 
+    $('.service').click(function() {
+        $('html,body').animate({ scrollTop: block1 }, 1000)
+        return false;
+    })
+    $('.contact').click(function() {
+        $('html,body').animate({ scrollTop: footer }, 1000)
+        return false;
+    })
+
 });
+
+function collapsMenu() {
+    $('header').css({
+        'background-color': '#ffffff20',
+        'backdrop-filter': 'blur(10px)',
+        '-webkit-backdrop-filter': 'blur(10px)',
+        '-moz-backdrop-filter': 'blur(10px)',
+        'height': '60px',
+    })
+}
+
+function expendMenu() {
+    $('header').css({
+        'background-color': 'transparent',
+        'backdrop-filter': 'blur(0px)',
+        '-webkit-backdrop-filter': 'blur(0px)',
+        '-moz-backdrop-filter': 'blur(0px)',
+        'height': '100px'
+    })
+}
